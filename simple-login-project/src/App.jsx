2f220ef50d773d09a03704627d521873c70933d9
yishizu <48244588+yishizu@users.noreply.github.com>
2023-04-25 19:31:17 +0900

    Update App.jsx

--- simple-login-project/src/App.jsx
+++ simple-login-project/src/App.jsx
@@ -6,22 +6,36 @@ import InputList from "./components/InputList";
 import InputForm from "./components/UI/InputForm";
 import { Box, Card } from "@mui/material";
 
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
 
 function App() {
   const [list, setList] = useState([]);
   const [name, setName] = useState("Your Name");
   const [age, setAge] = useState(0);
 
+  const [showAlert, setError] = useState(false);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(name, age);
-    setName("");
-    setAge(0);
-    setList([...list, { name, age }]);
-  };
+    
+    if(name.trim().length === 0 || age.length === 0){
+      setError(true);
+    }
+    else{
+      
+      setName("");
+      setAge(0);
+      setList([...list, { name, age }]);
+      setError(false);
+    }
+      
+    }
 
   return (
     <>
+      {showAlert && (<Alert onClose={() => setError(false)} severity="error">This is an error alert — check it out!</Alert>)}
       <InputForm
         name={name}
         age={age}
